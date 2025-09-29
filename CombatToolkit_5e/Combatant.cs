c10f3fa8b14f21a98769f6e95076184bc29e008b
Caleb Shook <syshook@wccnet.edu>
2025-09-29 18:41:41 -0400

    First "Working Version". Program sorts the list of combatants as you add them. No exception handling. Total compliance expected from the user.

---
+++ CombatToolkit_5e/Combatant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatToolkit_5e
+{
+    public class Combatant
+    {
+        // Instance variables (fields)
+        private string name;
+        private int initiative;
+
+        // Constructor
+        public Combatant(string name, int initiative)
+        {
+            this.name = name;
+            this.initiative = initiative;
+        }
+
+        // Public properties for access
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Initiative
+        {
+            get { return initiative; }
+            set { initiative = value; }
+        }
+        public override string ToString()
+        {
+            return name + " " + initiative.ToString();        
+        }
+    }
+}
--- CombatToolkit_5e/MainWindow.xaml.cs
+++ CombatToolkit_5e/MainWindow.xaml.cs
@@ -11,14 +11,45 @@ using System.Windows.Shapes;
 
 namespace CombatToolkit_5e
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
+    /// TODO Add data binding
+    /// TODO implement ObservableList<Combatant>
+    /// TODO implement ListBox or ListView (research them both)
+    /// TODO implement ViewModel
+    /// TODO Create menu bar
+    /// TODO Implement custom controls (kinda like fragments in android studio)
+    /// TODO resolve initiative disputes (possibly through pop up menu)
+    /// Make sure keyboard shortcuts are optimized for smooth workflow
     public partial class MainWindow : Window
     {
+
+        public List<Combatant> combatants = [];
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddCombatantButton_Click(object sender, RoutedEventArgs e)
+        {
+            //get string from textbox
+            Combatant newEntry = new(NameTextBox.Text, int.Parse(InitiativeTextBox.Text));
+
+            //add string to list of combatants
+            combatants.Add(newEntry);
+
+            //order the list by initiative
+            combatants.Sort((a, b) => b.Initiative.CompareTo(a.Initiative));
+
+            //clear the initiativeTextBlock
+            InitiativeTextBlock.Text = "";
+
+            //loop through the combatants and add them to initiativeTextBlock
+            foreach(Combatant combatant in combatants)
+            {
+                InitiativeTextBlock.Text += combatant.ToString() + "\n";
+            }
+
         }
     }
 }
