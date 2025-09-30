a92d81706333e81f8255ddba0301089bee982715
Caleb Shook <syshook@wccnet.edu>
2025-09-30 16:03:25 -0400

    This version implemented data binding and ListView. A remove button was added. Code was cleaned up a little. It no longer functions because it doesn't sort the combatants for you

--- CombatToolkit_5e/MainWindow.xaml.cs
+++ CombatToolkit_5e/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.DirectoryServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,44 +14,90 @@ using System.Windows.Shapes;
 namespace CombatToolkit_5e
 {
     
-    /// TODO Add data binding
     /// TODO implement ObservableList<Combatant>
-    /// TODO implement ListBox or ListView (research them both)
     /// TODO implement ViewModel
     /// TODO Create menu bar
     /// TODO Implement custom controls (kinda like fragments in android studio)
     /// TODO resolve initiative disputes (possibly through pop up menu)
-    /// Make sure keyboard shortcuts are optimized for smooth workflow
     public partial class MainWindow : Window
     {
 
-        public List<Combatant> combatants = [];
-
         public MainWindow()
         {
+            DataContext = this;
+            combatants = new ObservableCollection<string>();
             InitializeComponent();
         }
 
+        private ObservableCollection<string> combatants;
+
+        public ObservableCollection<string> Combatants
+        {
+            get { return combatants; }
+            set { combatants = value; }
+        }
+
+
         private void AddCombatantButton_Click(object sender, RoutedEventArgs e)
         {
-            //get string from textbox
-            Combatant newEntry = new(NameTextBox.Text, int.Parse(InitiativeTextBox.Text));
-
-            //add string to list of combatants
-            combatants.Add(newEntry);
-
-            //order the list by initiative
-            combatants.Sort((a, b) => b.Initiative.CompareTo(a.Initiative));
-
-            //clear the initiativeTextBlock
-            InitiativeTextBlock.Text = "";
-
-            //loop through the combatants and add them to initiativeTextBlock
-            foreach(Combatant combatant in combatants)
+            if(verifyInputs())
             {
-                InitiativeTextBlock.Text += combatant.ToString() + "\n";
-            }
+                //get string from textbox
+                Combatant newCombatant = new(NameTextBox.Text, int.Parse(InitiativeTextBox.Text));
 
+                //add string to list of combatants
+                combatants.Add(newCombatant.ToString());
+
+                NameTextBox.Clear();
+                InitiativeTextBox.Clear();
+                NameTextBox.Focus();
+            }
         }
+
+        private void RemoveCombatantButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                combatants.RemoveAt(InitiativeListView.SelectedIndex);
+            }
+            catch 
+            {
+                errorLabel.Content = "You must select a combatant to remove";
+            }
+        }
+
+        public bool verifyInputs()
+        {
+            errorLabel.Content = string.Empty;
+
+            string name = NameTextBox.Text;
+            if (name != "")
+            {
+                //name is good
+                if (int.TryParse(InitiativeTextBox.Text, out int successfulInitiative))
+                {
+                    //both are good
+                    return true;
+                }
+                else
+                {
+                    //just the init is bad
+                    errorLabel.Content = "Please enter a valid initiative";
+                }
+            }
+            else if (int.TryParse(InitiativeTextBox.Text, out int successfulInitiative))
+            {
+                //just the name is bad
+                errorLabel.Content = "Please enter a name";
+            }
+            else
+            {
+                //both are bad
+                errorLabel.Content = "Please enter a name and valid initiative";
+            }
+            return false;
+        }
+
+        
     }
 }
